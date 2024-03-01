1fd862fdac9ccf74cada1cb5ab035a0c67a1f1b3
Javohir <abdualiyevjavohirn1@gmail.com>
2024-03-01 08:37:58 +0500

    added flag

--- config/settings.py
+++ config/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 
 
 LANGUAGES = (
-    ('uz', _('Uzbek')),
-    ('en', _('English')),
-    ('ru', _('Russian')),
-    ('tr', _('Turkiye')),
+    ('uz', _('🇺🇿Uzbek')),
+    ('en', _('🇺🇸English')),
+    ('ru', _('🇷🇺Russian')),
+    ('tr', _('🇹🇷Turkiye')),
 )
 
 LOCALE_PATHS = [
