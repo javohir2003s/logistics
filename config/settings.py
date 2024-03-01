4512856f4547556ec402bf9d1217d4b153638453
Javohir <abdualiyevjavohirn1@gmail.com>
2024-03-01 12:36:28 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -34,13 +34,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://murodttlogistic.com/', 'http://127.0.0.1:8080/']
+CSRF_TRUSTED_ORIGINS = ['https://murodttlogistic.com', 'http://127.0.0.1:8080/']
 
 
-CSRF_COOKIE_DOMAIN = ['https://murodttlogistic.com/', 'http://127.0.0.1:8080/']
+CSRF_COOKIE_DOMAIN = ['https://murodttlogistic.com', 'http://127.0.0.1:8080/']
 
 CORS_ORIGIN_WHITELIST = (
-   'https://murodttlogistic.com/',
+   'https://murodttlogistic.com',
    'http://127.0.0.1:8080/',
    
 )
