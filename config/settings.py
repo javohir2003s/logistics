03d84088d1845a7978ca25615030ff89f0900be4
Javohir <abdualiyevjavohirn1@gmail.com>
2024-03-01 09:52:25 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
 CSRF_TRUSTED_ORIGINS = ['https://murodttlogistic.com/']
+
+
+CSRF_COOKIE_DOMAIN = 'https://murodttlogistic.com/'
+
+CORS_ORIGIN_WHITELIST = (
+   'https://murodttlogistic.com/'
+)
 
 # Application definition
 
